99ecd3db9044d10bc96d9a84c1f4c32fbfc688ee
Vincent Mahnke <vincent@mahn.ke>
2018-12-16 15:38:21 +0100

    Moved PingPong-test utility to separate, excluded file

--- src/__tests__/SessionServer.singleuser.test.ts
+++ src/__tests__/SessionServer.singleuser.test.ts
@@ -1,56 +1,8 @@
-import {SessionServer} from './SessionServer';
+// testing helper
+import {PingPong} from './PingPong.testutil';
 
-type resolveCallback = (regexMatches : RegExpMatchArray) => void;
-type rejectCallback = () => void;
-type resolveHandler = (message : MessageEvent) => void;
-type rejectHandler = () => void;
-class PingPong {
-    private readonly sentMessage : string;
-    private readonly expectedResponse : RegExp;
-    private readonly client : WebSocket;
-    private readonly isMatch : boolean;
-    private resolveMethod : resolveHandler;
-    private rejectMethod : rejectHandler;
-
-    public constructor(client : WebSocket, ping : string, pong : RegExp, isMatch : boolean) {
-        this.client = client;
-        this.sentMessage = ping;
-        this.expectedResponse = pong;
-        this.isMatch = isMatch;
-        this.resolveMethod = (message : MessageEvent) : void => {}; // tslint:disable-line:no-empty
-        this.rejectMethod = () : void => {}; // tslint:disable-line:no-empty
-    }
-
-    public async Execute() : Promise<RegExpMatchArray> {
-        return new Promise<RegExpMatchArray>((resolve : resolveCallback, reject : rejectCallback) : void => {
-            this.client.addEventListener('message', this.handleMessage(resolve, reject));
-            this.client.addEventListener('close', this.handleClose(resolve, reject));
-            this.client.send(this.sentMessage);
-        });
-    }
-    private handleMessage(resolve : resolveCallback, reject : rejectCallback) : resolveHandler {
-        this.resolveMethod = (message : MessageEvent) : void => {
-            this.client.removeEventListener('message', this.resolveMethod);
-            if (this.isMatch) {
-                expect(message.data).toMatch(this.expectedResponse);
-            } else {
-                expect(message.data).not.toMatch(this.expectedResponse);
-            }
-
-            resolve(<RegExpMatchArray>(<string>message.data).match(this.expectedResponse));
-        };
-
-        return this.resolveMethod;
-    }
-    private handleClose(resolve : resolveCallback, reject : rejectCallback) : rejectHandler {
-        this.rejectMethod = () : void => {
-            this.client.removeEventListener('close', this.rejectMethod);
-            reject();
-        };
-
-        return this.rejectMethod;
-    }
-}
+// testee
+import {SessionServer} from '../SessionServer'; // tslint:disable-line: no-relative-imports
 
 describe('SessionServer single user session', () => {
 
